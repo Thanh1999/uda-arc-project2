56ee74eaf0a1202555529e7a46888eefa9be117f
Thanh1999 <t.max347056@gmail.com>
2023-07-24 09:10:36 +0700

    update blabla

--- Task 5/Exercise 1/main.tf
+++ Task 5/Exercise 1/main.tf
@@ -8,4 +8,16 @@ resource "aws_instance" "Udacity-T2" {
     ami = "ami-04823729c75214919"
     instance_type = "t2.micro"
     count = 4
+    tags = {
+        "Name" = "Udacity T2"
+    }
+}
+
+resource "aws_instance" "Udacity-M4" {
+    ami = "ami-04823729c75214919"
+    instance_type = "m4.large"
+    count = 2
+    tags = {
+        "Name" = "Udacity M4"
+    }
 }
